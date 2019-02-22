891e2a1e593bfbde69cad5ceea71246188a3e6a3
omkarkakade <omkar710@gmail.com>
2019-02-22 16:45:14 -0500

    Adding stuff

--- src/services/FAQAnswerService.js
+++ src/services/FAQAnswerService.js
@@ -12,4 +12,32 @@ export default class FAQAnswerService {
     findAllFAQAnswers = () =>
         fetch(`${process.env.REACT_APP_MIDDLE_TIER_URL}/api/faq-answers`)
             .then(response => response.json())
+
+    updateFAQAnswers = frequentlyAskedAnswer =>
+        fetch(`http://localhost:8080/api/faq-answer/${frequentlyAskedAnswer.id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            // "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: JSON.stringify(frequentlyAskedAnswer), // body data type must match "Content-Type" header
+        })
+        .then(response => response.json()); // parses response to JSON
+
+    createFAQAnswers = frequentlyAskedAnswer =>
+        fetch(`http://localhost:8080/api/faq-answer`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                // "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: JSON.stringify(frequentlyAskedAnswer), // body data type must match "Content-Type" header
+        })
+            .then(response => response.json()); // parses response to JSON
+
+    deleteFAQAnswers = id =>
+        fetch(`http://localhost:8080/api/faq-answer/${id}`, {
+            method: "DELETE",
+        })
+            .then(response => response.json()); // parses response to JSON
 }
