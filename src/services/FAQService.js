085ba371f3a40f9f3be5d5b3e444bf1982eb539b
michaelkhalil <khalil.m@husky.neu.edu>
2019-03-21 23:18:05 -0400

    CRUD service for FAQs

--- src/services/FAQService.js
+++ src/services/FAQService.js
@@ -12,4 +12,35 @@ export default class FAQService {
     findAllFAQs = () =>
         fetch(`${process.env.REACT_APP_MIDDLE_TIER_URL}/api/faq`)
             .then(response => response.json())
+
+    updateFAQs = faq =>
+        // fetch(`http://localhost:8080/api/faq-answer/${frequentlyAskedAnswer.id}`, {
+        fetch(`${process.env.REACT_APP_MIDDLE_TIER_URL}/api/faq/${faq.id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                // "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: JSON.stringify(faq), // body data type must match "Content-Type" header
+        })
+            .then(response => response.json()); // parses response to JSON
+
+    createFAQs = faq =>
+        // fetch(`http://localhost:8080/api/faq-answer`, {
+        fetch(`${process.env.REACT_APP_MIDDLE_TIER_URL}/api/faq`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                // "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: JSON.stringify(faq), // body data type must match "Content-Type" header
+        })
+            .then(response => response.json()); // parses response to JSON
+
+    deleteFAQs = id =>
+        // fetch(`http://localhost:8080/api/faq-answer/${id}`, {
+        fetch(`${process.env.REACT_APP_MIDDLE_TIER_URL}/api/faq/${id}`, {
+            method: "DELETE",
+        })
+            .then(response => response.json()); // parses response to JSON
 }
