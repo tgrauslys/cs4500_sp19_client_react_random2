a08bd13770940290106b8f6405d045cd1b3a7c7f
jsteilberg <steilberg.j@husky.neu.edu>
2019-03-23 13:30:07 -0400

    Added delete button, create button to service category summary screen

--- src/components/ServiceCategories.js
+++ src/components/ServiceCategories.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ServiceCategoryService from '../services/ServiceCategoryService'
-import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Link, Redirect, Route} from 'react-router-dom'
+
 // Component that creates a list of ServiceCategories
 class ServiceCategories extends React.Component {
     constructor(props) {
@@ -10,38 +11,57 @@ class ServiceCategories extends React.Component {
             serviceCategories: []
         }
     }
+
     componentDidMount() {
         this.serviceCategoryService
             .findAllServiceCategories()
             .then(serviceCategories =>
-                  this.setState({
-                                    serviceCategories: serviceCategories
-                                })
+                      this.setState({
+                                        serviceCategories: serviceCategories
+                                    })
             )
     }
 
     // Create table with service categories
     render() {
-        return(
+
+        return (
             <div>
                 <h3>Service Categories</h3>
+                <a role="button" className="btn btn-success" variant="outline-success"
+                   onClick={ () =>
+                       this.serviceCategoryService.createServiceCategory()
+                           .then((response) => {
+                               this.props.history.push(`/admin/categories/${response['id']}`);
+                           })
+                   }>Create new</a>
                 <table className="table">
                     <tbody>
                     {
                         this.state.serviceCategories
                             .map(serviceCategory =>
-                                 <tr key={serviceCategory.id}>
-                                     <td>
-                                         <Link to={`/admin/categories/${serviceCategory.id}`}>
-                                             {serviceCategory.serviceCategoryName}
-                                         </Link>
-                                     </td>
-
-                                 </tr>
+                                     <tr key={serviceCategory.id}>
+                                         <td>
+                                             <Link to={`/admin/categories/${serviceCategory.id}`}>
+                                                 {serviceCategory.serviceCategoryName}
+                                             </Link>
+                                         </td>
+                                         <td>
+                                             <a role="button" className="btn btn-danger"
+                                                onClick={() => {
+                                                    this.serviceCategoryService.deleteServiceCategoryById(
+                                                        serviceCategory.id)
+                                                        .then(() => this.componentDidMount());
+                                                }}>
+                                                 Delete
+                                             </a>
+                                         </td>
+                                     </tr>
                             )
                     }
                     </tbody>
                 </table>
+
             </div>
         )
     }
--- src/services/ServiceCategoryService.js
+++ src/services/ServiceCategoryService.js
@@ -13,9 +13,31 @@ export default class ServiceCategoryService {
 
     // Lambdas to make REST calls
     findServiceCategoryById = categoryId =>
-        fetch(`${process.env.REACT_APP_MIDDLE_TIER_URL}/api/categories/${categoryId}`)
-            .then(response => response.json())
+        fetch(`${process.env.REACT_APP_MIDDLE_TIER_URL}api/categories/${categoryId}`)
+            .then(response => response.json());
     findAllServiceCategories = () =>
-        fetch(`${process.env.REACT_APP_MIDDLE_TIER_URL}/api/categories`)
-            .then(response => response.json())
+        fetch(`${process.env.REACT_APP_MIDDLE_TIER_URL}api/categories`)
+            .then(response => response.json());
+
+    deleteServiceCategoryById = categoryId =>
+        fetch(`${process.env.REACT_APP_MIDDLE_TIER_URL}api/categories/${categoryId}`, {
+            method: 'DELETE',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            }
+        });
+
+    createServiceCategory = () =>
+        fetch(`${process.env.REACT_APP_MIDDLE_TIER_URL}api/categories`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                serviceCategoryName: "",
+                services: []
+                                 })}).then(response => response.json());
+
 }
