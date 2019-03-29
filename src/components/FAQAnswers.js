c6196a992011ebfaeb25e5bd393604daee1a0a4f
tgrauslys <grauslys.t@husky.neu.edu>
2019-03-29 14:18:49 -0400

    Stateless refactor. Created component still need container

--- src/components/FAQAnswers.js
+++ src/components/FAQAnswers.js
@@ -1,5 +1,51 @@
 import React from 'react'
+import {Link} from "react-router-dom";
 
-const FAQAnswers = {}
+const FAQAnswers =
+    ({faqAnswers, faqAnswer, selectFaqAnswer, updateForm, createFaqAnswer, updateFaqAnswer, deleteFAQAnswer})=>
+        <div>
+            <h3> FAQ Answers {faqAnswers.length}</h3>
+            <table className="table">
+                <tbody>
+                <tr>
+                    <td>
+                        <input
+                            className="question-answer-fld"
+                            onChange = {e => updateForm(e)}
+                            value={faqAnswer.answer}></input>
+                        <button
+                            className="create-faq-answer-btn"
+                            onClick={createFaqAnswer}>+</button>
+                        <button
+                            className="update-faq-answer-btn"
+                            onClick={updateFaqAnswer}>Update</button>
+                    </td>
+                </tr>
+                {
+                    faqAnswers.map(faqAnswer =>
+                        <tr key={faqAnswer.id}>
+                            <td>{faqAnswer.question}</td>
+                            <td>
+                                <Link to={`/admin/faq-answers/${faqAnswer.id}`}>
+                                    {faqAnswer.answer}
+                                </Link>
+                            </td>
+                            <td>
+                                <a class="btn btn-danger btn-lg active" role="button" aria-pressed="true"
+                                   onClick={() =>
+                                       deleteFAQAnswer(faqAnswer.id)}
+                                >X</a>
+
+                            <button className="select-faq-answer-btn"
+                                    onClick={() => selectFaqAnswer(faqAnswer.id)}>
+                                Select
+                                </button>
+                            </td>
+                        </tr>
+                    )
+                }
+                </tbody>
+            </table>
+        </div>
 
 export default FAQAnswers
--- src/containers/FaqAnswerContainer.js
+++ src/containers/FaqAnswerContainer.js
@@ -13,125 +13,36 @@ class FAQAnswerContainer extends React.Component {
                 question: "",
                 answer: "",
             },
+            page: 0,
+            totalPages: 0,
+            elementsPerPage: 5
         }
 
     }
 
-    updateFormAnswer = e => this.setState({
+    updateForm = e => this.setState({
         editForm: {
             question: this.state.editForm.question,
             answer: e.target.value
         }
     })
 
-    updateFormQuestion = e => this.setState({
-        editForm: {
-            question: e.target.value,
-            answer: this.state.editForm.answer
-        }
-    })
+    findFAQAnswers = () =>
+        this.faqAnswerService.findFAQAnswerPage(this.state.page, this.state.elementsPerPage).then(faqAnswers => {
+            this.setState(
+                {faqAnswers: faqAnswers.content,
+                    totalPages: faqAnswers.totalPages
+                })
+        })
 
 
-    componentWillMount() {
 
-    }
+    selectPage = id => this.setState({page: totalPages})
 
     componentDidMount() {
-        this.faqAnswerService.findFAQAnswerPage(this.state.page, this.state.elementsInPage).then(faqAnswers =>
-            this.setState({
-                faqAnswers: faqAnswers.content,
-                lastPage: faqAnswers.last,
-            })
-        )
 
     }
 
-
-    deleteFAQAnswers = (id) => {
-        this.faqAnswerService
-            .deleteFAQAnswers(id).then(()=>{
-
-        })
-    }
-
-    // deleteFAQAnswers = (id) => {
-    //     this.faqAnswerService
-    //         .deleteFAQAnswers(id).then(()=>{
-    //         window.location.href='/admin/faq-answers';
-    //     });
-    // };
-
-    render() {
-        return (
-            <div>
-                <h3>FAQ Answers</h3>
-                <table className="table">
-                    <tr>
-                        <th> Question</th>
-                        <th> Answer</th>
-                    </tr>
-                    <tbody>
-                    <tr>
-                        <td>
-                            <input
-                                onChange={e => this.updateFormQuestion(e)}
-                            />
-                        </td>
-                        <td>
-                            <input
-                                onChange={e => this.updateFormAnswer(e)}
-                            />
-                        </td>
-
-                        <td>
-                            <button
-                                type="button" className="btn btn-primary"
-                                onClick={() => {
-                                    this.faqAnswerService.createFAQAnswers(this.state.editForm)
-                                }}
-                            >+
-                            </button>
-                        </td>
-
-                        <td>
-                            <button
-                                type="button" className="btn btn-success">Search
-                            </button>
-                        </td>
-
-
-                    </tr>
-
-                    {
-                        this.state.faqAnswers
-                            .map(faqAnswer =>
-                                <tr key={faqAnswer.id}>
-                                    <td>{faqAnswer.question}</td>
-                                    <td>
-                                        <Link to={`/admin/faq-answers/${faqAnswer.id}`}>
-                                            {faqAnswer.answer}
-                                        </Link>
-                                    </td>
-                                    <td>
-                                        <a class="btn btn-danger btn-lg active" role="button" aria-pressed="true"
-                                           onClick={() => {
-                                               this.deleteFAQAnswers(faqAnswer.id)
-                                           }}
-                                        >X</a>
-                                    </td>
-                                    <td>
-                                        {/*<button type="button" className="btn btn-warning">Edit</button>*/}
-                                        <a class="btn btn-outline-warning btn-lg active" role="button"
-                                        >Edit</a>
-                                    </td>
-                                </tr>
-                            )
-                    }
-                    </tbody>
-                </table>
-            </div>
-        )
-    }
 
 }
 
