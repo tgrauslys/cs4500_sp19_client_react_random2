1a770ceb4a43b7cf8baf9c6606630a6f9c489f1f
Alan Thamthieng <ithamthieng@gmail.com>
2019-03-30 01:58:09 -0400

    Added page number buttons and max-min limit

--- src/components/ServiceQuestions.js
+++ src/components/ServiceQuestions.js
@@ -8,19 +8,21 @@ class ServiceQuestions extends React.Component {
         this.state = {
             serviceQuestions: [],
             currentPage: 0,
-            itemCount: 10
+            itemCount: 10,
+            totalPages: 1
         }
     }
     setPage(e, pageNumber) {
         console.log(this.state.serviceQuestions)
 		const itemCount = (e && e.target && e.target.value) ? e.target.value : this.state.itemCount
 		this.serviceQuestionService.findQuestionPage(pageNumber, itemCount)
-            .then(serviceQuestions =>{
+            .then(serviceQuestions => {
+                console.log(serviceQuestions)
                 this.setState({
                     serviceQuestions: serviceQuestions.content,
 					currentPage: pageNumber,
-                    itemCount: itemCount
-                    
+                    itemCount: itemCount,
+                    totalPages: serviceQuestions.totalPages
                 })})
     }
     componentDidMount() {
@@ -33,6 +35,33 @@ class ServiceQuestions extends React.Component {
             )
     }
     render() {
+        const isFirstPage = this.state.currentPage === 0;
+        const isLastPage = this.state.currentPage >= this.state.totalPages - 1;
+
+        let previousButton = <button onClick= {(e) => this.setPage(e, this.state.currentPage - 1)}
+                                style={{margin: '2px'}}
+                                // &#60; displays "<"
+                                type="button" className="btn btn-secondary">&#60;</button>
+        let previousPageButton = <button onClick= {(e) => this.setPage(e, this.state.currentPage - 1)}
+                                    style={{margin: '2px'}}
+                                    type="button" className="btn btn-primary">{this.state.currentPage}</button>
+        let nextButton = <button onClick= {(e) => this.setPage(e, this.state.currentPage + 1)}
+                                style={{margin: '2px'}}
+                                // &#62; displays ">"
+                                type="button" className="btn btn-secondary">&#62;</button>
+        let nextPageButton = <button onClick= {(e) => this.setPage(e, this.state.currentPage + 1)}
+                                    style={{margin: '2px'}}
+                                    type="button" className="btn btn-primary">{this.state.currentPage + 2}</button>
+
+        if (isFirstPage) {
+            previousButton = ''
+            previousPageButton = ''
+        } 
+        if (isLastPage) {
+            nextButton = ''
+            nextPageButton = ''
+        }
+
         return(
             <div>
                 <h3>Service Questions</h3>
@@ -61,13 +90,13 @@ class ServiceQuestions extends React.Component {
                                     <option value="25">25</option>
                                     <option value="50">50</option>
                                 </select>
-                                <label style={{margin: '10px'}}>Page: {this.state.currentPage + 1}</label>
+                                {previousButton}
+                                {previousPageButton}
                                 <button
-                                    onClick= {(e) => this.setPage(e, this.state.currentPage - 1)}
-                                    type="button" className="btn btn-primary">Prev</button>
-                                <button
-                                    onClick= {(e) => this.setPage(e, this.state.currentPage + 1)} 
-                                    type="button" className="btn btn-primary">Next</button>
+                                    style={{margin: '2px'}}
+                                    type="button" className="btn btn-primary" disabled>{this.state.currentPage + 1}</button>
+                                {nextPageButton}
+                                {nextButton}
                             </td>
                         </tr>
                     </tbody>
