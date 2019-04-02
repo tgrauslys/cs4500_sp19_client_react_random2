4dbe6a92317e8a3e22d2656c9830ac26daabe4ba
jsteilberg <steilberg.j@husky.neu.edu>
2019-04-02 18:14:21 -0400

    Added user-side service category viewer, fixed some of admin side service category details

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import Admin from './components/Admin'
-import Home from './components/Home'
+import Home from './components/HomeScreen/Home'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import Provider from './components/Provider/Provider'
 import ServiceCategoryService from "./services/ServiceCategoryService";
+import CategoryList from "./components/HomeScreen/CategoryList";
+import CatListContainer from "./containers/CatListContainer";
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.serviceCategoryService = ServiceCategoryService.getInstance();
         this.state = {
-            pillServiceCategories: []
+            pillServiceCategories: [],
+            category: []
         }
+        this.findServiceCategories()
     }
 
     componentDidMount() {
@@ -23,10 +27,16 @@ class App extends Component {
         return this.serviceCategoryService
             .findAllServiceCategories()
             .then(serviceCategories =>
-                this.setState({
-                    pillServiceCategories: serviceCategories
-                })
+                      this.setState({
+                                        pillServiceCategories: serviceCategories
+                                    })
             )
+    }
+
+    findCategoryById = (id) => {
+        return this.serviceCategoryService
+            .findServiceCategoryById(parseInt(id)).then(category =>
+                                                            this.setState({category: category}))
     }
 
     render() {
@@ -51,7 +61,16 @@ class App extends Component {
                         <Route
                             path="/home"
                             exact
-                            render={() => <Home pillServiceCategories={this.state.pillServiceCategories}/>}/>
+                            render={() => <Home
+                                pillServiceCategories={this.state.pillServiceCategories}/>}/>
+
+                        <Route
+                            path="/categories/:id"
+                            exact
+                            render={(props) =>
+                                <CatListContainer
+                                    props = {props}
+                                    service = {this.serviceCategoryService}/>}/>
                     </div>
                 </Router>
 
