bbbbc13315c6af5ccfb6bdacb9d965e7204d332f
bachmai <bach.mai.viet@gmail.com>
2019-04-04 22:09:29 -0400

    created service category section page

--- src/components/ServiceNavigator/ServiceCategorySection.js
+++ src/components/ServiceNavigator/ServiceCategorySection.js
@@ -1,23 +1,27 @@
 import React from 'react'
 import ServiceCards from './ServiceCards'
+
 const ServiceCategorySection = ({serviceCategory}) =>
     <div>
         <a id={serviceCategory.id}/>
-        <h2>{serviceCategory.title}</h2>
-            <div>
-                <ServiceCards
-                    services={serviceCategory.services.slice(0,4)}/>
-                <div className="row">
-                    {
-                        serviceCategory.services.map(service =>
-                            <div key={service.id}
-                                className="col-6 list-group-item no-border">
-                                <a href="/providers"> {service.title}</a>
-                            </div>
-                        )
-                    }
-                </div>
+        <h2>{serviceCategory.serviceCategoryName}</h2>
+        <div>
+            {/*<ServiceCards*/}
+            {/*services={serviceCategory.serviceCategoryName.slice(0,4)}/>*/}
+            <img src={`https://picsum.photos/300/200?image=${Math.floor(Math.random() * 100)}`}
+                 className="card-img-top"/>
+            <br/>
+            <div className="row">
+                {
+                    serviceCategory.services.map(service =>
+                        <div key={service.id}
+                             className="col-6 list-group-item no-border">
+                            <a href="/provider"> {service.serviceName}</a>
+                        </div>
+                    )
+                }
             </div>
+        </div>
     </div>
 
 export default ServiceCategorySection
