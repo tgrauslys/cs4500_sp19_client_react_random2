87ef1ddd17696f605480c75393e5a171f841cd26
bachmai <bach.mai.viet@gmail.com>
2019-04-11 21:50:16 -0400

    add pagination to service summary

--- src/components/Services.js
+++ src/components/Services.js
@@ -2,41 +2,93 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 const Services = ({
-                               services,
-                               createService,
-                               deleteService,
-                           }) =>
+                      services,
+                      createService,
+                      deleteService,
+                      optionValues,
+                      currentPage,
+                      itemCount,
+                      totalPages,
+                      setPage
+                  }) => {
+    const isFirstPage = currentPage === 0;
+    const isLastPage = currentPage >= totalPages - 1;
 
-    <div>
-        <h3>Services</h3>
-        <a role="button" className="btn btn-success" variant="outline-success"
-           onClick={createService}
-        >Create New Service</a>
-        <table className="table">
-            <tbody>
-            {
-                services
-                    .map(services =>
-                        <tr key={services.id}>
-                            <td>
-                                <Link to={`/admin/services/${services.id}`}>
-                                    {services.serviceName}
-                                </Link>
-                            </td>
-                            <td>
-                                <a role="button" className="btn btn-danger"
-                                   onClick={() => {
-                                       deleteService(
-                                           services.id)
-                                   }}>
-                                    Delete
-                                </a>
-                            </td>
-                        </tr>
-                    )
-            }
-            </tbody>
-        </table>
-    </div>
+    let previousButton = <button onClick={(e) => setPage(e, currentPage - 1)}
+                                 style={{margin: '2px'}}
+                                 type="button" className="btn btn-secondary">&#60;</button>
+    let previousPageButton = <button onClick={(e) => setPage(e, currentPage - 1)}
+                                     style={{margin: '2px'}}
+                                     type="button" className="btn btn-primary">{currentPage}</button>
+    let nextButton = <button onClick={(e) => setPage(e, currentPage + 1)}
+                             style={{margin: '2px'}}
+                             type="button" className="btn btn-secondary">&#62;</button>
+    let nextPageButton = <button onClick={(e) => setPage(e, currentPage + 1)}
+                                 style={{margin: '2px'}}
+                                 type="button" className="btn btn-primary">{currentPage + 2}</button>
+
+    if (isFirstPage) {
+        previousButton = ''
+        previousPageButton = ''
+    }
+    if (isLastPage) {
+        nextButton = ''
+        nextPageButton = ''
+    }
+
+    return (
+        <div>
+            <h3>Services</h3>
+            <a role="button" className="btn btn-success" variant="outline-success"
+               onClick={createService}
+            >Create New Service</a>
+            <table className="table">
+                <tbody>
+                {
+                    services ?
+                    services
+                        .map(services =>
+                            <tr key={services.id}>
+                                <td>
+                                    <Link to={`/admin/services/${services.id}`}>
+                                        {services.serviceName}
+                                    </Link>
+                                </td>
+                                <td>
+                                    <a role="button" className="btn btn-danger"
+                                       onClick={() => {
+                                           deleteService(
+                                               services.id)
+                                       }}>
+                                        Delete
+                                    </a>
+                                </td>
+                            </tr>
+                        ) : "Loading services..."
+                }
+                <tr>
+                    <td>
+                        {console.log(optionValues)}
+                        <select value={itemCount} onChange={e => setPage(e)}>
+                            {
+                                optionValues.map(possibleItemCounts =>
+                                    <option key={possibleItemCounts}
+                                            value={possibleItemCounts}>{possibleItemCounts}</option>)
+                            }
+                        </select>
+                        {previousButton}
+                        {previousPageButton}
+                        <button
+                            style={{margin: '2px'}}
+                            type="button" className="btn btn-primary" disabled>{currentPage + 1}</button>
+                        {nextPageButton}
+                        {nextButton}
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
 
 export default Services
