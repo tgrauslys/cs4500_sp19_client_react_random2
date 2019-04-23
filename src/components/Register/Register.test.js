b2070a9610f737851942ceb90b184588ef0988d9
Eric A. Martineau <emartineau52@gmail.com>
2019-04-23 16:39:05 -0400

    changes test to include setup-teardown

--- src/components/Register/Register.test.js
+++ src/components/Register/Register.test.js
@@ -1,93 +1,79 @@
-import registry from '../data/Register.mock.json'
+import registry from '../../data/Register.mock.json'
 import React from 'react'
 import Register from './Register'
 import TestRenderer from 'react-test-renderer'
-import UserService from '../services/UserService.mock'
+import UserService from '../../services/UserService'
+import Profile from '../Profile/Profile'
 
 const userService = UserService.getInstance()
+var testUserId
 
-test('render register screen', () => {
-    let updateFirstName = () => {}
-    let updateLastName = () => {}
-    let updateEmail = () => {}
-    let updateUsername = () => {}
-    let updatePassword = () => {}
-    let submit = () => {}
-
-    const testRenderer = TestRenderer.create(
-        <Register 
-            register={submit}
-            updateFirstName={updateFirstName}
-            updateLastName={updateLastName}
-            updateEmail={updateEmail}
-            updateUsername={updateUsername}
-            updatePassword={updatePassword}
-            isErrorMessageOn={true}/>
-    )
-
-    let tree = testRenderer.toJSON()
-    expect(tree).toMatchSnapshot()
-
-    const testInstance = TestRenderer.root
-
-    const firstNameField = testInstance.findByProps({id: "first-name"})
-    const lastNameField = testInstance.findByProps({id: "last-name"})
-    const emailField = testInstance.findByProps({id: "email"})
-    const usernameField = testInstance.findByProps({id: "username"})
-    const passwordField = testInstance.findByProps({id: "password"})
-    const registerButton = testInstance.findAllByProps({className: "btn btn-primary btn-block"})
-
-    expect(firstNameField.onChange).toBeDefined()
-    expect(lastNameField.onChange).toBeDefined()
-    expect(emailField.onChange).toBeDefined()
-    expect(usernameField.onChange).toBeDefined()
-    expect(passwordField.onChange).toBeDefined()
-    expect(registerButton[0].onClick).toBeDefined()
-    expext(registerButton.length).toBe(1)
-})
-
-test('click register to sign up', (done) => {
-    let updateFirstName = () => {}
-    let updateLastName = () => {}
-    let updateEmail = () => {}
-    let updateUsername = () => {}
-    let updatePassword = () => {}
-    let submit = () => {
-        userService.register(registry.validInput)
-        let tree = testRenderer.toJSON
-        expect(tree).toMatchSnapshot()
-        done()
-    }
-
-    const TestRenderer = TestRenderer.create(
-        <Register
-            register={submit}
-            updateFirstName={updateFirstName}
-            updateLastName={updateLastName}
-            updateEmail={updateEmail}
-            updateUsername={updateUsername}
-            updatePassword={updatePassword}
-            isErrorMessageOn={true}/>)
-
-    let tree = testRenderer.toJSON
-    expect(tree).toMatchSnapshot()
+describe('Register Page Test', () => {
+    beforeAll(() => {
+        let updateFirstName = () => {}
+        let updateLastName = () => {}
+        let updateEmail = () => {}
+        let updateUsername = () => {}
+        let updatePassword = () => {}
+        let submit = () => {
+            testUserId = userService.register(registry.validInput).then(user => user.id)
+            let tree = testRenderer.toJSON()
+            expect(tree).toMatchSnapshot()
+            done()
+        }
     
-    expect(firstNameField.onChange).toBeDefined()
-    expect(lastNameField.onChange).toBeDefined()
-    expect(emailField.onChange).toBeDefined()
-    expect(usernameField.onChange).toBeDefined()
-    expect(passwordField.onChange).toBeDefined()
-    expect(registerButton.onClick).toBeDefined()
+        const testRenderer = TestRenderer.create(
+            <Register 
+                register={submit}
+                updateFirstName={updateFirstName}
+                updateLastName={updateLastName}
+                updateEmail={updateEmail}
+                updateUsername={updateUsername}
+                updatePassword={updatePassword}
+                isErrorMessageOn={true}/>
+        )
+    
+        let tree = testRenderer.toJSON()
+        expect(tree).toMatchSnapshot()
+    
+        const testInstance = testRenderer.root
+    
+        const firstNameField = testInstance.findByProps({id: "first-name"})
+        const lastNameField = testInstance.findByProps({id: "last-name"})
+        const emailField = testInstance.findByProps({id: "email"})
+        const usernameField = testInstance.findByProps({id: "username"})
+        const passwordField = testInstance.findByProps({id: "password"})
+        const registerButton = testInstance.findAllByProps({className: "btn btn-primary btn-block"})
+    
+        expect(firstNameField.onChange).toBeDefined()
+        expect(lastNameField.onChange).toBeDefined()
+        expect(emailField.onChange).toBeDefined()
+        expect(usernameField.onChange).toBeDefined()
+        expect(passwordField.onChange).toBeDefined()
+        expect(registerButton[0].onClick).toBeDefined()
+        expect(registerButton.length).toBe(1)
+    
+        registerButton.props.onClick()
+    })
 
-    registerButton.props.onClick()
+    test('user is registered', () => {
+        expect(userService.findUserById(testUserId)).toBe(registry.validInput)
+    })
+    
+    test('profile screen is displayed once registered', (done) => {
+        let updateUserProfile = () => {}
+        let deleteUserProfile = () => {}
+
+        const testRenderer = TestRenderer.create(<Profile 
+            user={registry.validInput}
+            updateUserProfile={updateUserProfile}
+            deleteUserProfile={deleteUserProfile}/>)
+    })
+    
+    afterEach(() => {
+        if (testUserId) {
+            userService.deleteUserById(testUserId)
+        }
+        testUserId = undefined
+    })
 })
-
-test('profile screen is displayed once registered'), (done) => {
-    let updateUserProfile = () => {}
-    let deleteUserProfile = () => {}
-
-    const testRenderer = TestRenderer.create(<Profile 
-        user={registry.validInput}
-        updateUserProfile={updateUserProfile}
-        deleteUserProfile={deleteUserProfile}/>)
-}
